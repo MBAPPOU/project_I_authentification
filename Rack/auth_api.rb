e28bbb4bd524bb283d140309539ff719dec3370b
Yvan MBAPPOU <yvan.mbappou@enseeiht.fr>
2012-03-06 12:57:26 +0000

    rack d'authentification d'utilisateurs

--- Rack/auth_api.rb
+++ Rack/auth_api.rb
@@ -1,9 +1,9 @@
 # Rack chargé d'authentifier un utilisateur
 $:.unshift File.dirname(__FILE__)
-require '../user' 
+require_relative '../user'
+require_relative '../database'
 
 class AuthApi
-include User
 
   def initialize(app)
     @app = app
@@ -11,57 +11,76 @@ include User
     @indice = 0
   end
 
+  def self.add(word)
+      @sessions[@indice]=word
+  end
+
   def call(env)
     request = Rack::Request.new(env)
+    status, headers, body = @app.call(env)
+    #puts "-------------------"
+    #puts request.form_data?()
+    #puts "-------------------"
+    #puts request.POST()
+    #puts "-------------------"
+    # On recupère les paramètres du formulaire
+    if ( request.form_data?() )
+       @login = request.POST()["login"] 
+       @password = request.POST()["password"]
+       headers["logpass"] = [ @login , @password ].join(':')
+    else
+       @login = nil
+       @password = nil
+    end
     #--------------------------------------------------------------------------------------------------------------------------
     # On verifie si l'environnement de la requête a un cookie
     #--------------------------------------------------------------------------------------------------------------------------
     cookieset = request.cookies() # On recupère un tableau de cookies s'il en existe ou pas
-    status, headers, body = @app.call(env)
     headers["authentification"] = "unknow" # Par défaut aucune session en cours
-    if cookieset # Si 'env' a un ou des cookies, on les compare au tableau de sessions initialisées
-    cookieset.each { |n|
-       @sessions.each { |k, v|
-          if ( cookieset[n] = v ) # Si la correspondance est faite, alors l'utilisateur a une session en cours
-             headers["authentification"] = "true"
-             break
-          end 
-       }
-       break if ( headers["authentification"] == "true" )
-    }
+    if ( cookieset || (@login && @password) )# Si 'env' a un ou des cookies, on les compare au tableau de sessions initialisées
+          if ( cookieset )
+              cookieset.each { |n,m|
+                 @sessions.each { |k, v|
+                     if ( m = v ) # Si la correspondance est faite, alors l'utilisateur a une session en cours
+                         headers["authentification"] = "true"
+                     break
+                     end 
+                 }
+                 break if ( headers["authentification"] == "true" )
+              }
+          end
+          if ( @login && @password )
+              # On crée un modèle d'utiisateur doté des paramètres récupérés
+              p = User.new
+              if p 
+              p.login = @login
+              p.password = @password if p
+              # On vérifie si l'utilisateur existe bien dans la base de donnée
+              q = find(p.login)
+              if ( q ) # Si l'utilisateur existe
+                   if ( p.login == q.login  && p.password == q.password ) # si l'authentification est correcte, on valide
+                       @indice += 1
+                       val = "name=generate_session_id"
+                       headers["Set-Cookie"] = [ env["Set-Cookie"], "#{val}" ].join(';') 
+                       @sessions[@indice] = "#{val}" # On enregistre le cookie dans sessions
+                       headers["authentification"] = "true"
+                   else  # sinon on invalide car l'authentification s'est mal déroulée
+                       headers["authentification"] = "false"
+                   end
+              else # l'utilisateur est inconnu ou il s'agit d'une demande de création de compte
+                   if ( headers["authentification"] == "unknow" && request.POST()["message"] == "createaccount" )
+                       p.save
+                       headers["authentification"] = "true"
+                   else
+                       headers["authentification"] = "false"
+                   end
+              end
+              end
+          end
     else
     #--------------------------------------------------------------------------------------------------------------------------
-    # L'environnement ici n'a pas de cookie,donc on traite soit une demande de connexion,soit une demande de création de compte
+    # L'environnement ici n'a pas de cookie et aucun paramètres login et mdp
     #--------------------------------------------------------------------------------------------------------------------------
-        # On recupère les paramètres de l'environnement
-        parametres = request.params[]
-        @login = parametres[login]
-        @password = parametres[password]
-    
-        # On crée un modèle d'utiisateur doté des paramètres récupérés
-        p = User.new
-        p.login = @login
-        p.password = @password
-    
-        # On vérifie si l'utilisateur existe bien dans la base de donnée
-        q = find(p.login)
-    
-        if q # Si l'utilisateur existe
-            if ( p.login == q.login  && p.password == q.password ) # si l'authentification est correcte, on valide
-               headers["authentification"] = "true"
-               @index += 1
-               val = generate_session_id 
-               headers["Set-Cookie"] = "name=#{val}" # On initialise un cookie de session
-               @sessions[@index] = "name=#{val}" # On enregistre le cookie
-            else  # sinon on invalide car l'authentification s'est mal déroulée
-               headers["authentification"] = "false"
-            end
-        else # l'utilisateur est inconnu ou il s'agit d'une demande de création de compte
-           if ( headers["authentification"] == "unknow" && parametres[message] == "createaccount" )
-              p.save
-              headers.delete["authentification"]
-           end
-        end
     end
      [status, headers, body]
   end
@@ -71,7 +90,7 @@ include User
   end
   
   def find(word)
-     User.find_by_name(word.to_s())
+     User.find_by_name( word.to_s() )
   end
   
 end
